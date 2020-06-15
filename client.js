2f7a98987c711822611deef3c0ed2bab22088a01
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-06-15 10:05:58 +0200

    finish second task, fetch data dynamically, add new data dynamically

--- client.js
+++ client.js
@@ -29,10 +29,12 @@ const state = {
     topic_details: "",
     expected_result: "",
     target_level: "",
-  }
+  }, 
+  requests: "",
 }
 
 // Methods: 
+
 const sendRequest = (e)=>{
   e.preventDefault();
  fetch('http://localhost:7777/video-request', {
@@ -43,7 +45,31 @@ const sendRequest = (e)=>{
   body: JSON.stringify(state.form),
  })
 .then(response => response.json())
-.then(data => console.log(data));
+.then(request => {
+  addRequestDiv(request);
+});
 }
 
+const getRequests = (()=>{
+  fetch("http://localhost:7777/video-request")
+  .then(response => response.json())
+.then(data => {state.requests = data; showRequests()});
+})();
 
+
+const showRequests = ()=>{
+  for (request of state.requests){
+    addRequestDiv(request);
+  }
+};
+
+
+
+const addRequestDiv = (request)=>{
+  const htmlContent =  `<div class='card mb-3'><div class='card-body d-flex justify-content-between flex-row'><div class='d-flex flex-column'><h3>${request.topic_title}</h3><p class='text-muted mb-2'>${request.topic_details}</p><p class='mb-0 text-muted'><strong>Expected results:</strong>${request.expected_result}</p></div><div class='d-flex flex-column text-center'><a class='btn btn-link'>🔺</a><h3>0</h3><a class='btn btn-link'>🔻</a></div></div><div class='card-footer d-flex flex-row justify-content-between'><div><span class='text-info'>${request.status}</span>&bullet; added by <strong>${request.author_name}</strong> on<strong>${request.submit_date}</strong></div><div class='d-flex justify-content-center flex-column 408ml-auto mr-2'><div class='badge badge-success'>${request.target_level}</div></div></div></div>`;
+
+  const requestsDiv = document.getElementById("listOfRequests");
+  const requestNode = document.createElement("div");
+  const contentOfNode = requestNode.innerHTML = htmlContent;
+  requestsDiv.prepend(requestNode);
+}
